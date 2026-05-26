408dd943d1800f1e799a7f4ed89c06bca309ee1c
milhud <milhud@milhud>
2026-05-26 12:42:56 -0400

    eval: swap PREC for TD2, scan 150 patches for best inference plots

--- scripts/run_final_plots.sh
+++ scripts/run_final_plots.sh
@@ -15,23 +15,14 @@ cd /gpfsm/dnb33/hpmille1/diffusion_downscaling_model
 
 CACHE_DIR="/discover/nobackup/sduan/.data"
 
-echo "--- [1/2] 3-var inference plots (scan 80, 5-col, with axes) ---"
+echo "--- [1/2] 3-var inference plots (scan 150, 5-col, with axes) ---"
 python -m src.evaluation.quick_inference_plots \
   --num_samples 6 \
   --num_members 4 \
   --num_steps 16 \
-  --scan 80 \
-  --vars T2 U10 PREC_ACC_NC \
+  --scan 150 \
+  --vars T2 U10 TD2 \
   --output_dir results/inference_plots_3var \
-  --cache_dir "$CACHE_DIR"
-
-echo ""
-echo "--- [2/2] Power spectra (2x3 layout) ---"
-python -m src.evaluation.stage_spectra \
-  --output_dir results/spectra \
-  --num_steps 16 \
-  --num_ensemble 2 \
-  --max_batches 10 \
   --cache_dir "$CACHE_DIR"
 
 echo "Done: $(date)"
